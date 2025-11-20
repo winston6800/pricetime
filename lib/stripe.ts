0dbb092e7ad828f9070f2cb35f76720f7a02e623
winston6800 <47305958+winston6800@users.noreply.github.com>
2025-11-19 20:46:05 -0800

    Update Stripe API version to match package

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -5,7 +5,7 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2024-11-20.acacia',
+  apiVersion: '2025-02-24.acacia',
   typescript: true,
 });
 
